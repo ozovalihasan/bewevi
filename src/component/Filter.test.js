f280e50294188615aa252d4b83ab89fc6b40f1a1
Hasan Ozovali <ozovalihasan@gmail.com>
2020-12-21 00:02:56 +0300

    Update tests of Filter

--- src/component/Filter.test.js
+++ src/component/Filter.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import renderer from 'react-test-renderer';
 import Filter from './Filter';
@@ -12,38 +12,38 @@ let filter;
 let filterInput;
 
 describe('<Filter />', () => {
-  // it('shows correct options, not input and filter icon', () => {
-  //   filter = 'all';
-  //   filterInput = '';
-  //   render(<Filter
-  //     handleChangeFilter={handleChangeFilter}
-  //     plantProperties={plantProperties}
-  //     handleChangeInput={handleChangeInput}
-  //     handleClick={handleClick}
-  //     filter={filter}
-  //     filterInput={filterInput}
-  //   />);
-  //   expect(screen.queryByDisplayValue('All')).toBeTruthy();
-  //   expect(screen.queryByPlaceholderText('Filter')).not.toBeTruthy();
-  //   expect(screen.queryByAltText('Filter Results')).not.toBeTruthy();
+  it('shows correct options, not input and filter icon', () => {
+    filter = 'all';
+    filterInput = '';
+    render(<Filter
+      handleChangeFilter={handleChangeFilter}
+      plantProperties={plantProperties}
+      handleChangeInput={handleChangeInput}
+      handleClick={handleClick}
+      filter={filter}
+      filterInput={filterInput}
+    />);
+    expect(screen.queryByDisplayValue('All')).toBeTruthy();
+    expect(screen.queryByPlaceholderText('Filter')).not.toBeTruthy();
+    expect(screen.queryByAltText('Filter Results')).not.toBeTruthy();
+  });
 
-  // });
+  it('shows correct options and input, not filter icon when any options except \'all\' is chosen ', () => {
+    filter = 'common_name';
+    filterInput = '';
+    render(<Filter
+      handleChangeFilter={handleChangeFilter}
+      plantProperties={plantProperties}
+      handleChangeInput={handleChangeInput}
+      handleClick={handleClick}
+      filter={filter}
+      filterInput={filterInput}
+    />);
+    expect(screen.queryByDisplayValue('Common Name')).toBeTruthy();
+    expect(screen.queryByPlaceholderText('Filter')).toBeTruthy();
+    expect(screen.queryByAltText('Filter Results')).not.toBeTruthy();
+  });
 
-  // it('shows correct options and input, not filter icon when any options except \'all\' is chosen ', () => {
-  //   filter = 'common_name';
-  //   filterInput = '';
-  //   render(<Filter
-  //     handleChangeFilter={handleChangeFilter}
-  //     plantProperties={plantProperties}
-  //     handleChangeInput={handleChangeInput}
-  //     handleClick={handleClick}
-  //     filter={filter}
-  //     filterInput={filterInput}
-  //   />);
-  //   expect(screen.queryByDisplayValue('Common Name')).toBeTruthy();
-  //   expect(screen.queryByPlaceholderText('Filter')).toBeTruthy();
-  //   expect(screen.queryByAltText('Filter Results')).not.toBeTruthy();
-  // });
   it('shows correct options, input and filter icon when input is not empty', () => {
     filter = 'common_name';
     filterInput = 'ivy';

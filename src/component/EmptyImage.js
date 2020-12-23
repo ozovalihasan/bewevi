a26a0dc91383e8d28adff4342196460c24a81bc1
Hasan Ozovali <ozovalihasan@gmail.com>
2020-12-23 14:17:19 +0300

    Update EmptyImage component

--- src/component/EmptyImage.js
+++ src/component/EmptyImage.js
@@ -1,6 +1,12 @@
+import PropTypes from 'prop-types';
 import emptyImageSVG from '../assets/emptyImage.svg';
 
 const EmptyImage = ({ className = '' }) => (
   <img src={emptyImageSVG} alt="Not provided" className={className} />
 );
+
+Error.propTypes = {
+  className: PropTypes.string.isRequired,
+};
+
 export default EmptyImage;
